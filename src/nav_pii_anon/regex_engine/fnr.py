ce25d4770aed64358af0f784efd1a808bfff031f
Petter Nymark <petternymark@Petters-MacBook-Pro.local>
2020-07-06 14:53:13 +0200

    Lagt til validering og distance

--- src/nav_pii_anon/regex_engine/fnr.py
+++ src/nav_pii_anon/regex_engine/fnr.py
@@ -4,6 +4,44 @@ import re
 import numpy as np
 
 
+def levenstein_distance(pnr):
+    """
+    :param pnr: Takes
+    :return: Levenstein distance of from a PNR without spaces
+    """
+    # TODO: Make this to check for distance to valid PNR
+
+    pnr = str(pnr)
+    regex_size = 11
+    size_x = len(pnr) + 1
+    size_y = regex_size + 1  # size of pnr
+    matrix = np.zeros((size_x, size_y))  # Empty matrix
+    valid_pnr = r""
+
+    for x in range(size_x):
+        matrix[x, 0] = x
+    for y in range(size_y):
+        matrix[0, y] = y
+
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if re.match(r'\d', pnr[x - 1]):
+                matrix[x, y] = min(
+                    matrix[x - 1, y] + 1,
+                    matrix[x - 1, y - 1],
+                    matrix[x, y - 1] + 1
+                )
+            else:
+                matrix[x, y] = min(
+                    matrix[x - 1, y] + 1,
+                    matrix[x - 1, y - 1] + 1,
+                    matrix[x, y - 1] + 1
+                )
+
+    print('The Levenstein distance from a pnr is {0}'.format(matrix[size_x - 1, size_y - 1]))
+    return matrix[size_x - 1, size_y - 1]
+
+
 class RegexFnr(RegexBase):
 
     @property
@@ -22,69 +60,83 @@ class RegexFnr(RegexBase):
     def score(self):
         return 1
 
-    def levenstein_distance(self, pnr):
-        """
-        :param pnr: Takes
-        :return: Levenstein distance of from a pnr without spaces
-        """
+    @property
+    def validate(self):
+        pass
 
-        regex_size = 11
-        size_x = len(pnr) + 1
-        size_y = regex_size + 1  # size of pnr
-        matrix = np.zeros((size_x, size_y))  # Empty matrix
+    def validate_pnr(self, pnr: str):
 
-        for x in range(size_x):
-            matrix[x, 0] = x
-        for y in range(size_y):
-            matrix[0, y] = y
+        if len(pnr) == 11 and re.match(r"\d{11}", pnr):
+            d1 = int(pnr[0])
+            d2 = int(pnr[1])
 
-        for x in range(1, size_x):
-            for y in range(1, size_y):
-                if re.match(r'\d', pnr[x - 1]):
-                    matrix[x, y] = min(
-                        matrix[x - 1, y] + 1,
-                        matrix[x - 1, y - 1],
-                        matrix[x, y - 1] + 1
-                    )
-                else:
-                    matrix[x, y] = min(
-                        matrix[x - 1, y] + 1,
-                        matrix[x - 1, y - 1] + 1,
-                        matrix[x, y - 1] + 1
-                    )
+            m1 = int(pnr[2])
+            m2 = int(pnr[3])
 
-        return matrix[size_x - 1, size_y - 1]
+            y1 = int(pnr[4])
+            y2 = int(pnr[5])
 
-        def validate(self):
-            pass
+            i1 = int(pnr[6])
+            i2 = int(pnr[7])
+            i3 = int(pnr[8])
 
-    ## TODO: implement with our format
+            k1 = int(pnr[9])
+            k2 = int(pnr[10])
 
-    # def validate(date: str, person_nr: str) -> None:
-    #     d1 = int(date[0])
-    #     d2 = int(date[1])
+            k1_mod = (3 * d1 + 7 * d2 + 6 * m1 + m2 + 8 * y1 + 9 * y2 + 4 * i1 + 5 * i2 + 2 * i3) % 11
+            new_k1 = 0 if k1_mod == 0 else (11 - k1_mod)
+
+            if k1 != new_k1:
+                print('not a valid PNR')
+                levenstein_distance(pnr)
+
+
+            k2_mod = (5 * d1 + 4 * d2 + 3 * m1 + 2 * m2 + 7 * y1 + 6 * y2 + 5 * i1 + 4 * i2 + 3 * i3 + 2 * k1) % 11
+            new_k2 = 0 if k2_mod == 0 else (11 - k2_mod)
+
+            if k2 != new_k2:
+                levenstein_distance(pnr)
+                print('not a valid PNR')
+
+            if k1 == new_k1 and k2 == new_k2:
+                print('Valid PNR')
+                return 1
+
+        else:
+            levenstein_distance(pnr)
+
+
+    # TODO: implement with our format
+
+    # def validate(pnr: str):
     #
-    #     m1 = int(date[2])
+    #
+    #     d1 = int(pnr[0])
+    #     d2 = int(pnr[1])
+    #
+    #     m1 = int(pnr[2])
     #     m2 = int(date[3])
     #
-    #     y1 = int(date[4])
-    #     y2 = int(date[5])
+    #     y1 = int(pnr[4])
+    #     y2 = int(pnr[5])
     #
-    #     i1 = int(person_nr[0])
-    #     i2 = int(person_nr[1])
-    #     i3 = int(person_nr[2])
+    #     i1 = int(pnr[6])
+    #     i2 = int(pnr[7])
+    #     i3 = int(pnr[8])
     #
-    #     k1 = int(person_nr[3])
-    #     k2 = int(person_nr[4])
+    #     k1 = int(pnr[9])
+    #     k2 = int(pnr[10])
     #
     #     k1_mod = (3 * d1 + 7 * d2 + 6 * m1 + m2 + 8 * y1 + 9 * y2 + 4 * i1 + 5 * i2 + 2 * i3) % 11
     #     new_k1 = 0 if k1_mod == 0 else (11 - k1_mod)
     #
     #     if k1 != new_k1:
-    #         raise ValueError
+    #         levenstein_distance(pnr)
     #
     #     k2_mod = (5 * d1 + 4 * d2 + 3 * m1 + 2 * m2 + 7 * y1 + 6 * y2 + 5 * i1 + 4 * i2 + 3 * i3 + 2 * k1) % 11
     #     new_k2 = 0 if k2_mod == 0 else (11 - k2_mod)
     #
     #     if k2 != new_k2:
     #         raise ValueError
+    #     else
+    #         print('This is PNR')
