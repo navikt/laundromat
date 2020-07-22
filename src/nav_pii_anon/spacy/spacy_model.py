70a0452cbadff36334e6709c4fadbb6c4e956718
Marius <mariusdioli@gmail.com>
2020-07-22 15:16:00 +0100

    Functioning custom scorer and f_1 scorer

--- src/nav_pii_anon/spacy/spacy_model.py
+++ src/nav_pii_anon/spacy/spacy_model.py
@@ -1,24 +1,21 @@
 
-from nav_pii_anon.spacy.matcher_regex import match_func
-from nav_pii_anon.spacy.matcher_list import name_list_matcher
-from spacy.matcher import Matcher
-from spacy import displacy
-
 import random
 import warnings
 from itertools import zip_longest
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
 import plac
 import spacy
-
+from nav_pii_anon.spacy.matcher_list import name_list_matcher
 from nav_pii_anon.spacy.matcher_regex import match_func
 from sklearn.metrics import f1_score
 from spacy import displacy
+from spacy.gold import GoldParse
 from spacy.matcher import Matcher
+from spacy.scorer import Scorer
 from spacy.util import compounding, minibatch
-from pathlib import Path
 
 
 class SpacyModel:
@@ -112,6 +109,18 @@ class SpacyModel:
             assert nlp2.get_pipe("ner").move_names == move_names
 
 
+    def f1_scorer(self, TEST_DATA):
+        scorer = Scorer()
+        df = pd.DataFrame(TEST_DATA)
+        df.columns = ["Text", "True_entities"]
+        for txt, ents in zip(df["Text"], df["True_entities"]):
+            doc = self.model.make_doc(txt)
+            gold = GoldParse(doc, entities=ents["entities"])
+            pred = self.get_doc(txt)
+            scorer.score(pred, gold)
+        return scorer.scores
+
+
 
     def test(self, TEST_DATA):
         """
@@ -128,11 +137,13 @@ class SpacyModel:
         score = 0
         number_of_ents = 0
         df = pd.DataFrame(TEST_DATA)
-        df.columns = ["Text", "True_entites"]
-        df["Model_entities"] = df["Text"].apply(lambda x: self.model(x).ents)
+        df.columns = ["Text", "True_entities"]
+        df["Model_entities"] = df["Text"].apply(lambda x: {"entities": [(ent.start, ent.end, ent.label_) for ent in self.model(x).ents]})
+        #print(df.head())
+        #print(self.predict("Test text Marius"))
         #Iterate through the prediction and the truth and compare
         #Generate binary list with values based on the truth and predictions
-        for model_ent, truth in zip_longest(df["Model_entities"], df["True_entites"]):
+        for model_ent, truth in zip_longest(df["Model_entities"], df["True_entities"]):
             if not truth:
                 y_true += [0]
                 y_pred += [1]
@@ -141,30 +152,39 @@ class SpacyModel:
                 y_pred += [0]
             else:
                 #Compares the model prediction with ground truth
-                model_start, model_end, model_label = model_ent['entities'][0][0], model_ent['entities'][0][1], model_ent['entities'][0][2]
-                truth_start, truth_end, truth_label = truth['entities'][0][0], truth['entities'][0][1], truth['entities'][0][2]
-                if model_start == truth_start and model_end == truth_end:
-                    #Checks if the entities are the same
-                    y_true += [1]
-                    y_pred += [1]
-                    score += 1
-                    number_of_ents += 1
-                elif (model_start < truth_end < model_end) or (model_end > truth_start > model_start):
-                    #Checks if there is exclusive overlap between the labels
-                    y_true += [1]
-                    y_pred += [0]
-                    score += (model_end-model_start)/(truth_end-truth_start)
-                    number_of_ents += 1
+                print("model_ent", model_ent)
+                if len(model_ent['entities'])==0:
+                    y_pred += [0] *len(truth['entities'])
+                    y_true += [1] *len(truth['entities'])
+                elif len(truth['entities'])==0:
+                    y_pred += [1] *len(model_ent['entities'])
+                    y_true += [0] *len(model_ent['entities'])
                 else:
-                    #This covers cases where a label is wholly contained in another
-                    if (model_end-model_start)< (truth_end-truth_start):
-                        score += 1
-                        y_true += [1]
-                        y_pred += [0]
-                    else:
-                        score += 0.5
+                    #TODO Make for loop to iterate over all entities in the list.
+                    model_start, model_end, model_label = model_ent['entities'][0][0], model_ent['entities'][0][1], model_ent['entities'][0][2]
+                    truth_start, truth_end, truth_label = truth['entities'][0][0], truth['entities'][0][1], truth['entities'][0][2]
+                    if model_start == truth_start and model_end == truth_end:
+                        #Checks if the entities are the same
                         y_true += [1]
                         y_pred += [1]
-                    number_of_ents += 1
+                        score += 1
+                        number_of_ents += 1
+                    elif (model_start < truth_end < model_end) or (model_end > truth_start > model_start):
+                        #Checks if there is exclusive overlap between the labels
+                        y_true += [1]
+                        y_pred += [0]
+                        score += (model_end-model_start)/(truth_end-truth_start)
+                        number_of_ents += 1
+                    else:
+                        #This covers cases where a label is wholly contained in another
+                        if (model_end-model_start)< (truth_end-truth_start):
+                            score += 1
+                            y_true += [1]
+                            y_pred += [0]
+                        else:
+                            score += 0.5
+                            y_true += [1]
+                            y_pred += [1]
+                        number_of_ents += 1
 
         return score/number_of_ents, f1_score(y_true, y_pred)
