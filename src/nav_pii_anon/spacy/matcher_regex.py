33e6ea6ad287b4638c2249f22c53c294bf247f40
Marius <mariusdioli@gmail.com>
2020-08-04 16:03:13 +0200

    mc

--- src/nav_pii_anon/spacy/matcher_regex.py
+++ src/nav_pii_anon/spacy/matcher_regex.py
@@ -18,7 +18,7 @@ def match_func(doc):
             spans.append(span)
             if not overlap(span, doc):
                 doc.ents = list(doc.ents) + [span]
-    if spans is not none:
+    if spans is not None:
         for span in spans:
             span.merge()
     return doc
