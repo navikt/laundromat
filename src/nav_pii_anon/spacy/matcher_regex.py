266f89fc491188aca4f0dc99e0c29691a0301779
Marius <mariusdioli@gmail.com>
2020-08-04 15:25:42 +0200

    Changed regex overlap

--- src/nav_pii_anon/spacy/matcher_regex.py
+++ src/nav_pii_anon/spacy/matcher_regex.py
@@ -11,64 +11,14 @@ def match_func(doc):
     """
     entity_list = regex_engines()
     for entity in entity_list:
-
         for match in re.finditer(entity.regex_pattern, doc.text):
             start, end = match.span()
             span = doc.char_span(start, end, label=entity.label)
-
             if not overlap(span, doc):
                 doc.ents = list(doc.ents) + [span]
-
     return doc
 
-
 def overlap(span, doc):
-    """
-    Checks if two entities overlap
-
-    :param span: A span of tokens, that the regex wants to match to a label
-    :param doc: The text in doc-format
-    :return: A boolean overlap indicator
-    """
-    if span is not None:
-        for ent in list(doc.ents):
-            if span.start in range(ent.start, ent.end + 1) or span.end in range(ent.start, ent.end + 1):
-                overlap_solver(span, doc, ent)
-                return True
-        return False    
-    else:
-        return True
-
-
-def overlap_solver(span, doc, ent):
-    """
-    The actual overlap solver. If the new entity covers more than the old one, it replaces the old one. If it is a
-    partial overlap, it extends both into a new one labeled <OVERLAP>
-
-    :param span: A span of tokens, that the regex wants to match to a label
-    :param doc: The text in doc-format
-    :param ent: The type of entity
-    """
-    if span.start == ent.start and span.end == ent.end:
-        #Keeps the previous entity and discards the new one
-        pass
-    elif (span.start < ent.end < span.end) or (span.end > ent.start > span.start):
-        #Creates a new entity with the label overlap
-        start = min(span.start, ent.start)
-        end = max(span.end, ent.end)
-        doc.ents.remove(ent)
-        new_span = doc.char_span(start, end, label="OVERLAP")
-        doc.ents = list(doc.ents) + [new_span]
-    else:
-        if len(span.text) > len(ent.text):
-            #Discards the old entity and replaces it with the new one
-            print(ent)
-            print(doc.ents)
-            new_list = list(doc.ents)
-            new_list.remove(ent)
-            print(new_list)
-            print(new_list + [span])
-            doc.ents = new_list + [span]
-            print("====")
-        else:
-            pass
+    for entity in doc.ents:
+        if (entity.start <= span.start <= entity.end) or (entity.start <=span.end <=entity.end):
+            return True
