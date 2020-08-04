1654db30e876a1e13eee24abf767315884034228
Marius <mariusdioli@gmail.com>
2020-08-04 15:47:24 +0200

    mc

--- src/nav_pii_anon/spacy/matcher_regex.py
+++ src/nav_pii_anon/spacy/matcher_regex.py
@@ -1,6 +1,6 @@
 import re
 from nav_pii_anon.spacy.regex_formatter import regex_engines
-
+from spacy.tokens import Doc, Span, Token
 
 def match_func(doc):
     """
@@ -10,12 +10,16 @@ def match_func(doc):
     :return: The text in doc-format with added entities
     """
     entity_list = regex_engines()
+    spans = []
     for entity in entity_list:
         for match in re.finditer(entity.regex_pattern, doc.text):
             start, end = match.span()
-            span = doc.char_span(start, end, label=entity.label)
+            span = Span(doc, start, end, label=entity.label)
+            spans.append(span)
             if not overlap(span, doc):
                 doc.ents = list(doc.ents) + [span]
+    for span in spans:
+        span.merge()
     return doc
 
 def overlap(span, doc):
