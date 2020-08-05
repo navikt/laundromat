9fdbc6d2d2b9f8dc98fe0262273ea55143f233a2
Petter Nymark <petternymark@Petters-MacBook-Pro.local>
2020-08-05 10:22:44 +0200

    Added ents_regex

--- src/nav_pii_anon/spacy/matcher_regex.py
+++ src/nav_pii_anon/spacy/matcher_regex.py
@@ -11,16 +11,15 @@ def match_func(doc):
     """
     entity_list = regex_engines()
     spans = []
+
     for entity in entity_list:
         for match in re.finditer(entity.regex_pattern, doc.text):
             start, end = match.span()
             span = doc.char_span(start, end, label=entity.label)
-            spans.append(span)
-            if not overlap(span, doc):
-                doc.ents = list(doc.ents) + [span]
-    for span in spans:
-        if span is not None:
-            span.merge()
+            if type(span) is not type(None):
+                spans.append(span)
+
+    doc._.ents_regex = spans
     return doc
 
 def overlap(span, doc):
